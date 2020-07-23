46d1e792de91c174536acf3cc2a3ff8a43b52285
arpitsaxena47 <51399299+arpitsaxena47@users.noreply.github.com>
2020-07-23 16:53:36 +0530

    next commit

--- app/src/main/java/com/arpit/bottomnavigationdemoapp/MainActivity.java
+++ app/src/main/java/com/arpit/bottomnavigationdemoapp/MainActivity.java
@@ -58,7 +58,6 @@ private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        LayoutInflater.from(this).inflate(R.layout.activity_main,null);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
